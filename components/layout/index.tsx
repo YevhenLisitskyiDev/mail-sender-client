920b0e020f52e244a8fb8f4f9a47b709b996109a
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-08-02 21:16:20 +0300

    Header Cmponent created

---
+++ components/layout/index.tsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+
+const Header: FC = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <IconButton edge="start" color="inherit" aria-label="menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6">Mail sender</Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
