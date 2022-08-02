1826da805d15c330c45778a369565bb4d92543da
YevhenLisitskyiDev <yevhen.lisitskyi.dev@gmail.com>
2022-08-02 19:15:19 +0300

    MUI setup added

---
+++ theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#041C32",
+      light: "#064663",
+    },
+    secondary: {
+      main: "#ECB365",
+    },
+  },
+});
